f3df02ffb1324917f44a210134679be8fee782ed
Emma <k54979802@gmail.com>
2016-08-26 09:11:49 +0000

    modal 更改

--- index.php
+++ index.php
@@ -49,7 +49,6 @@
         <!-- Modal content-->
         <div class="modal-content">
           <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal">&times;</button>
             <h4 class="modal-title"></h4>
           </div>
           <div class="modal-footer">
